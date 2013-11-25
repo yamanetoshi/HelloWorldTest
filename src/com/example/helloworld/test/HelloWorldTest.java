7eb7862f326463ff4233c3d18c526b04b26f4973
YAMANE Toshiaki <yamanetoshi@gmail.com>
2013-11-26 08:08:13 +0900

    Fix import

--- src/com/example/helloworld/test/HelloWorldTest.java
+++ src/com/example/helloworld/test/HelloWorldTest.java
@@ -2,20 +2,43 @@ package com.example.helloworld.test;
 
 import com.example.helloworld.MainActivity;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
-import com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions;
-import com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers;
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onData;  
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;  
+import static com.google.android.apps.common.testing.ui.espresso.Espresso.pressBack;  
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;  
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.closeSoftKeyboard;  
+import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.typeText;  
+import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;  
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;  
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;  
+import static org.hamcrest.Matchers.allOf;  
+import static org.hamcrest.Matchers.containsString;  
+import static org.hamcrest.Matchers.instanceOf;  
+import static org.hamcrest.Matchers.is; 
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.test.ActivityInstrumentationTestCase2;
 
 public class HelloWorldTest extends ActivityInstrumentationTestCase2<MainActivity> {
 	
+	private Activity mActivity;
+	private Instrumentation mInstrumentation;
+	
 	public HelloWorldTest() {
 		super(MainActivity.class);
 	}
+	
+	@Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mActivity = getActivity();
+        mInstrumentation = getInstrumentation();
+        setActivityInitialTouchMode(false);
+    }      
 
 	public void testHelloWorld() {
-		onView(ViewMatchers.withId(com.example.helloworld.R.id.helloworld))
-		.check(ViewAssertions.matches(ViewMatchers.withText("Hello World!")));
+		onView(withId(com.example.helloworld.R.id.helloworld))
+		.check(matches(withText("Hello World!")));
 	}
 }
